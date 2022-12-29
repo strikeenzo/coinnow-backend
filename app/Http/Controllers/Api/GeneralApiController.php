43e5f1f788ab17130594ef97a27e5d11ac6297d8
bowingman <rabbit@sky.io>
2022-12-29 07:16:49 -0500

    Fixed

--- app/Http/Controllers/Api/GeneralApiController.php
+++ app/Http/Controllers/Api/GeneralApiController.php
@@ -154,7 +154,7 @@ function getTendencyfromQuantity($quantity, $store_quantity, $auto_stock_amount)
         return 0.3;
     }
     if ($quantity > 5 and $quantity > $auto_stock_amount * 3 / 4) {
-        if (rand(0, 10) > 5) {
+        if (rand(0, 10) > 4) {
             return 1;
         }
         return -2;
